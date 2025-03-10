714deeed771ace4c3389cd00c4e26454ae1c7227
Tommasodibertomancini <tommydbm@gmail.com>
2025-03-10 15:32:42 +0100

    Aggiungere i file di progetto.

---
+++ BE-Biblioteca/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BE_Biblioteca.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
